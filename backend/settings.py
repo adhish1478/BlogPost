eced25c2cef3736bd888bf0a16c050a6c0559360
adhish1478 <adhish.aravind7@gmail.com>
2025-07-24 18:15:36 +0530

    initial setup- new apps and basic routing

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts', # accounts app
+    'blogs', # blog app
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
