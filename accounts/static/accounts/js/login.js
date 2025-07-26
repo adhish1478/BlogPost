53d53fc6b359f3aa68f76f321bb7ce196088d4c3
adhish1478 <adhish.aravind7@gmail.com>
2025-07-27 02:37:41 +0530

    user detail view added, routed, saved username and id inside localstorage

--- accounts/static/accounts/js/login.js
+++ accounts/static/accounts/js/login.js
@@ -22,6 +22,9 @@ document.getElementById('form').addEventListener('submit', async function(e) {
         localStorage.setItem('access', data.access);
         localStorage.setItem('refresh', data.refresh);
 
+        // Fetch and store user info
+        await fetchUserInfo();
+
         // Redirect to home
         //alert('Login successful! Redirecting to home...');
         window.location.href = '/';
@@ -29,3 +32,19 @@ document.getElementById('form').addEventListener('submit', async function(e) {
         alert(data.detail || 'Login failed');
     }
 });
+
+async function fetchUserInfo() {
+    const host = "http://localhost:8000/api";
+    const res= await fetch(`${host}/me`, {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("access")}`
+        }
+    });
+    if (res.ok) {
+        const user = await res.json();
+        localStorage.setItem("user_id", user.id);
+        localStorage.setItem("username", user.username);
+    } else {
+        alert("Failed to fetch user data.");
+    }
+};
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RegisterView, register_page, login_page
+from .views import RegisterView, MeView, register_page, login_page
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,6 +10,7 @@ urlpatterns=[
     path('api/token/', TokenObtainPairView.as_view(), name= 'token'),
     path('api/refresh/', TokenRefreshView.as_view(), name= 'token-refresh'),
     path('api/logout/', TokenBlacklistView.as_view(), name= 'logout'),
+    path('api/me/', MeView.as_view(), name='me-api'),
     # render html urls
     path('register/', register_page, name='register-page'),
     path('login/', login_page, name='login-page'),
